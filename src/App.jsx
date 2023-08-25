3b1b719069aedab1ce909da6f1f285882d99b6b7
Dona-D32 <115752283+Dona-D32@users.noreply.github.com>
2023-08-24 20:17:09 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Creatures from './components/Creatures';
 import CreatureDetails from './components/CreatureDetails';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+//import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ function App() {
           <Route path="/creatures/:id" component={CreatureDetails} />
         </Switch>
       </Router>
-      <ReactQueryDevtoolsPanel isOpen={false} />
+     {/* <ReactQueryDevtoolsPanel isOpen={false} />*/}
     </QueryClientProvider>
   );
 }
